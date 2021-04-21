25534bd4caf262af71c5bcd243ab58cd51b4a856
Michael Strobl <mstrobl@ualberta.ca>
2021-04-20 21:29:32 -0600

    Add files via upload
    added shell files for both entity linkers

---
+++ annotate_basic.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "start title2Id_redirect_parser"
+python src/title2Id_redirect_parser.py
+echo "start article_parser_1"
+python src/article_parser_1.py
+echo "start dicts_creator"
+python src/dicts_creator.py
+echo "start article_parser_2"
+python src/article_parser_2.py
+echo "start article_parser_3_basic"
+python src/article_parser_3_basic.py
+echo "start article_parser_4"
+python src/article_parser_4.py

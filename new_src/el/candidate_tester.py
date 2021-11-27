b42d91c4a43fdadfde2a4c62b4dc9f92b31399f4
Michael Strobl <mstrobl@ualberta.ca>
2021-11-27 10:41:13 +0100

    better candidate selection

--- new_src/el/candidate_tester.py
+++ new_src/el/candidate_tester.py
@@ -10,36 +10,35 @@ from tqdm import tqdm
 import numpy as np
 from model import BertForEntityClassification
 from transformers import (
-    AdamW,BertTokenizer, BertForNextSentencePrediction
+    AdamW, BertTokenizer, BertForNextSentencePrediction
 )
-
 
 RE_LINKS = re.compile(r'\[{2}(.*?)\]{2}', re.DOTALL | re.UNICODE)
 
 MAX_SENT_LENGTH = 256
 MAX_ABSTRACT_LENGTH = 64
 
-
-
-
 MAX_NUM_CANDIDATES = 50
 
 ABSTRACTS = {}
 
+
 class OurDataset(torch.utils.data.Dataset):
     def __init__(self, encodings):
         self.encodings = encodings
+
     def __getitem__(self, idx):
         return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+
     def __len__(self):
         return len(self.encodings.input_ids)
 
 
-def get_abstract(title,title2filename, entity_title2filename):
+def get_abstract(title, title2filename, entity_title2filename):
     if title in ABSTRACTS:
         return ABSTRACTS[title]
     try:
-        with open(title2filename[title].replace("articles_2","articles_3")) as f:
+        with open(title2filename[title].replace("articles_2", "articles_3")) as f:
             for line in f:
                 line = line.strip()
                 if len(line) > 0:
@@ -62,7 +61,7 @@ def get_abstract(title,title2filename, entity_title2filename):
                             entities = fe.read()
 
                         abstract_parts = line.split(' ')
-                        line = ' '.join(abstract_parts[:min(MAX_ABSTRACT_LENGTH,len(abstract_parts))])
+                        line = ' '.join(abstract_parts[:min(MAX_ABSTRACT_LENGTH, len(abstract_parts))])
                         line += ' ' + entities
 
                     ABSTRACTS[title] = line
@@ -97,35 +96,133 @@ def create_candidate_set(candidates, title2id, type=''):
 
     result = {}
     for key in candidates:
-        for i in range(min(current_max_cands, len(candidates[key]['list']))):
-            candidate = candidates[key]['list'][i]
-            if candidate[0] in title2id:
-                name = candidate[0]
-                prior = candidate[1]
-                redirect = 0
-                surname = 0
-                if prior == -1:
-                    redirect = 1
-                    prior = 0.0
-                elif prior == -2:
-                    surname = 1
-                    prior = 0.0
+        if key == 'document_mention':
+            r = len(candidates[key])
+        else:
+            r = len(candidates[key]['list'])
+            if 'lower' in key:
+                r = min(current_max_cands, len(candidates[key]['list']))
+
+            if type == 'train':
+                r = min(current_max_cands, len(candidates[key]['list']))
+
+        for i in range(r):
+            if key == 'document_mention':
+                name = candidates[key][i][0]
+                prior = candidates[key][i][1]
+                redirect = candidates[key][i][2]
+                surname = candidates[key][i][3]
+                document_mention = candidates[key][i][4]
 
                 if name in result:
-                    result[name][1] = max(prior,result[name][1])
+                    result[name][1] = max(prior, result[name][1])
                     result[name][2] = max(redirect, result[name][2])
                     result[name][3] = max(surname, result[name][3])
+                    result[name][4] = max(document_mention, result[name][4])
                 else:
-                    result[name] = [name,prior,redirect,surname]
+                    result[name] = [name, prior, redirect, surname, document_mention]
+            else:
+                candidate = candidates[key]['list'][i]
+                if candidate[0] in title2id:
+                    name = candidate[0]
+                    prior = candidate[1]
+                    redirect = 0
+                    surname = 0
+                    document_mention = 0
+                    if key == 'document_mentions':
+                        prior = 0.0
+                        document_mention = 1
+                    elif prior == -1:
+                        redirect = 1
+                        prior = 0.0
+                    elif prior == -2:
+                        surname = 1
+                        prior = 0.0
+
+                    if name in result:
+                        result[name][1] = max(prior, result[name][1])
+                        result[name][2] = max(redirect, result[name][2])
+                        result[name][3] = max(surname, result[name][3])
+                        result[name][4] = max(document_mention, result[name][4])
+                    else:
+                        result[name] = [name, prior, redirect, surname, document_mention]
 
     result_l = []
     for name in result:
-        result_l.append((name,result[name][1],result[name][2],result[name][3]))
+        result_l.append((name, result[name][1], result[name][2], result[name][3], result[name][4]))
 
     return result_l
 
 
-def process(documents,id2title, title2id, title2filename, entity_title2filename, redirects, person_candidates, priors_lower, tokenizer=None, type=''):
+def get_candidates(mention_upper, id2title, title2id, redirects, person_candidates, priors_lower, document_mentions,type=''):
+    mention = unidecode(mention_upper).lower()
+    mention_parts = mention_upper.split()
+    mention_lower_cleaned = ''
+    surname = ''
+    for part in mention_parts:
+        if len(part) == 2 and part[1] == '.':
+            mention_lower_cleaned += ''
+        else:
+            mention_lower_cleaned += ' ' + unidecode(part).lower()
+            surname = part
+
+    if len(surname) > 1:
+        surname = surname[0].upper() + surname[1:].lower()
+
+    mention_lower_cleaned = mention_lower_cleaned.strip()
+
+    type_found = []
+    candidates = {}
+
+    if mention in priors_lower:
+        type_found.append('priors lower')
+        candidates['priors_lower'] = priors_lower[mention]
+
+    if mention_lower_cleaned in priors_lower:
+        type_found.append('mention_lower_cleaned')
+        candidates['mention_lower_cleaned'] = priors_lower[mention_lower_cleaned]
+
+    if "ue" in mention:
+        mention = mention.replace('ue', "u")
+
+    if mention not in priors_lower:
+
+        if "oe" in mention:
+            mention = mention.replace('oe', "o")
+
+        if mention not in priors_lower:
+            if "ae" in mention:
+                mention = mention.replace('ae', "a")
+
+    if mention in priors_lower:
+        type_found.append('priors lower cleaned')
+        candidates['priors_lower_cleaned'] = priors_lower[mention]
+
+    if mention_upper in redirects:
+        type_found.append('redirects')
+        candidates['redirects'] = {"dict": {redirects[mention_upper]: -1},
+                                   'list': [(redirects[mention_upper], -1)]}
+
+    found_in_document_mentions = False
+    for document_mention in document_mentions:
+        if mention in document_mention:
+            type_found.append('document mentions')
+            found_in_document_mentions = True
+            candidates['document_mention'] = document_mentions[document_mention]
+            break
+
+    if not found_in_document_mentions and surname in person_candidates:
+        type_found.append('surname')
+        candidates['surname'] = person_candidates[surname]
+
+
+    candidates = create_candidate_set(candidates, title2id, type=type)
+
+    return candidates
+
+
+def process(documents, id2title, title2id, title2filename, entity_title2filename, redirects, person_candidates,
+            priors_lower, tokenizer=None, type=''):
     labels = []
     contexts = []
     abstracts = []
@@ -134,15 +231,14 @@ def process(documents,id2title, title2id, title2filename, entity_title2filename,
     adds_redirects = []
     adds_surname = []
 
-    test_data = {"ids":[], "contexts":[], "candidates":[]}
+    test_data = {"ids": [], "contexts": [], "candidates": []}
 
     correct_found = 0
+    correct_not_found = 0
     all = 0
     not_found = 0
 
-
-
-    counter = {1:0,0:0}
+    counter = {1: 0, 0: 0}
 
     for document in documents:
         document_mentions = {}
@@ -160,111 +256,51 @@ def process(documents,id2title, title2id, title2filename, entity_title2filename,
                 all += 1
                 start = tuple[0]
                 id = tuple[1]
+
+
+
                 end = tuple[2]
                 mention_upper = ' '.join(sentence[start:end])
 
                 if id in id2title:
-                    mention = unidecode(' '.join(sentence[start:end]).lower())
-
-                    context = ' '.join(sentence[:start]) + ' <e>' + ' '.join(sentence[start:end]) + '</e> ' + ' '.join(sentence[end:])
-                    context = context.strip()
-
-                    if len(sentence_before) + len(sentence) < MAX_SENT_LENGTH/2:
-                        sentence_before_str = ' '.join(sentence_before)
-                        #sentence_after_str = ' '.join(sentence_after)
-                        context = sentence_before_str + ' ' + context
-
-                        if len(sentence_before) + len(sentence) + len(sentence_after) < MAX_SENT_LENGTH/2:
-                            sentence_after_str = ' '.join(sentence_after)
-                            context += ' ' + sentence_after_str
-
-
-                    mention_parts = mention_upper.split()
-                    mention_lower_cleaned = ''
-                    surname = ''
-                    for part in mention_parts:
-                        if len(part) == 2 and part[1] == '.':
-                            mention_lower_cleaned += ''
-                        else:
-                            mention_lower_cleaned += ' ' + unidecode(part).lower()
-                            surname = part
-
-                    if len(surname) > 1:
-                        surname = surname[0].upper() + surname[1:].lower()
-
-                    mention_lower_cleaned = mention_lower_cleaned.strip()
-
-                    type_found = []
-                    candidates = {}
-
 
                     title = id2title[id]
 
                     if title in redirects:
                         title = redirects[title]
 
-                    if mention in priors_lower:
-                        type_found.append('priors lower')
-                        candidates['priors_lower'] = priors_lower[mention]
+                    mention = unidecode(' '.join(sentence[start:end]).lower())
 
-                    if mention_lower_cleaned in priors_lower:
-                        type_found.append('mention_lower_cleaned')
-                        candidates['mention_lower_cleaned'] = priors_lower[mention_lower_cleaned]
+                    context = ' '.join(sentence[:start]) + ' <e>' + ' '.join(sentence[start:end]) + '</e> ' + ' '.join(
+                        sentence[end:])
+                    context = context.strip()
 
-                    if "ue" in mention:
-                        mention = mention.replace('ue', "u")
+                    if len(sentence_before) + len(sentence) < MAX_SENT_LENGTH / 2:
+                        sentence_before_str = ' '.join(sentence_before)
+                        # sentence_after_str = ' '.join(sentence_after)
+                        context = sentence_before_str + ' ' + context
 
-                    if mention not in priors_lower:
+                        if len(sentence_before) + len(sentence) + len(sentence_after) < MAX_SENT_LENGTH / 2:
+                            sentence_after_str = ' '.join(sentence_after)
+                            context += ' ' + sentence_after_str
 
-                        if "oe" in mention:
-                            mention = mention.replace('oe', "o")
-
-                        if mention not in priors_lower:
-                            if "ae" in mention:
-                                mention = mention.replace('ae', "a")
-
-                    if mention in priors_lower:
-                        type_found.append('priors lower cleaned')
-                        candidates['priors_lower_cleaned'] = priors_lower[mention]
-
-                    if mention_upper in redirects:
-                        type_found.append('redirects')
-                        candidates['redirects'] = {"dict": {redirects[mention_upper]: -1},
-                                                   'list': [(redirects[mention_upper], -1)]}
-
-                    '''for document_mention in document_mentions:
-                        if mention in document_mention:
-                            type_found.append('document mentions')
-                            if 'priors_lower' in document_mentions[document_mention]:
-                                candidates['document_mentions'] = document_mentions[document_mention]['priors_lower']
-                            elif 'priors_lower_cleaned' in document_mentions[document_mention]:
-                                candidates['document_mentions'] = document_mentions[document_mention]['priors_lower_cleaned']
-                            elif 'redirects' in document_mentions[document_mention]:
-                                candidates['document_mentions'] = document_mentions[document_mention]['redirects']
-                            elif 'surname' in document_mentions[document_mention]:
-                                candidates['document_mentions'] = document_mentions[document_mention]['surname']
-
-                            break'''
-
-                    if surname in person_candidates:
-                        type_found.append('surname')
-                        candidates['surname'] = person_candidates[surname]
-
+                    candidates = get_candidates(mention_upper, id2title, title2id, redirects, person_candidates,
+                                                priors_lower, document_mentions, type=type)
 
                     if len(candidates) > 0:
-                        # document_mentions[mention] = candidates
+                        document_mentions[mention] = candidates
                         candidate_l = []
-                        candidates = create_candidate_set(candidates, title2id, type)
 
                         test_data['ids'].append(id)
                         test_data['contexts'].append(context)
 
+                        found = False
 
                         for j in range(len(candidates)):
                             candidate = candidates[j]
                             abstract = get_abstract(candidate[0],title2filename, entity_title2filename)
 
-                            # abstract = ''
+                            #abstract = ''
                             contexts.append(context)
                             abstracts.append(abstract)
                             prior = candidate[1]
@@ -275,15 +311,19 @@ def process(documents,id2title, title2id, title2filename, entity_title2filename,
                             adds_surname.append(surname)
                             adds_redirects.append(redirect)
 
-
-
                             if candidate[0] == title:
                                 labels.append(1)
-                                correct_found += 1
+                                found = True
                             else:
                                 labels.append(0)
                             counter[labels[-1]] += 1
                             candidate_l.append((candidate[0], prior, redirect, surname, abstract))
+
+                        if found:
+                            correct_found += 1
+                        else:
+                            correct_not_found += 1
+
 
                         test_data['candidates'].append(candidate_l)
 
@@ -299,6 +339,7 @@ def process(documents,id2title, title2id, title2filename, entity_title2filename,
                     test_data['contexts'].append(context)
                     test_data['candidates'].append([])
 
+
     print(counter)
     dataset = None
     if tokenizer != None:
@@ -312,12 +353,13 @@ def process(documents,id2title, title2id, title2filename, entity_title2filename,
 
     print("all: " + str(all))
     print("correct found: " + str(correct_found))
+    print("correct not found: " + str(correct_not_found))
     print("not found: " + str(not_found))
 
     return dataset, test_data
 
 
-def get_dataset(wexea_directory, tokenizer=None,type=''):
+def get_dataset(wexea_directory, tokenizer=None, type=''):
     fname = "data/" + type + ".pickle"
     if os.path.isfile(fname) and tokenizer != None:
         with open(fname, 'rb') as handle:
@@ -327,7 +369,6 @@ def get_dataset(wexea_directory, tokenizer=None,type=''):
         print("using pickled file.")
         return dataset, test_data
     else:
-
 
         id2title = json.load(open(wexea_directory + 'dictionaries/id2title.json'))
         title2id = json.load(open(wexea_directory + 'dictionaries/title2Id.json'))
@@ -369,9 +410,10 @@ def get_dataset(wexea_directory, tokenizer=None,type=''):
             if len(current_document) > 0:
                 documents.append(current_document)
 
-            #documents = documents[:10]
+            # documents = documents[:10]
 
-            dataset, test_data = process(documents, id2title, title2id, title2filename, entity_title2filename, redirects, person_candidates, priors_lower, tokenizer=tokenizer, type=type)
+            dataset, test_data = process(documents, id2title, title2id, title2filename, entity_title2filename,
+                                         redirects, person_candidates, priors_lower, tokenizer=tokenizer, type=type)
             with open(fname, 'wb') as handle:
                 pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
                 pickle.dump(test_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -380,16 +422,16 @@ def get_dataset(wexea_directory, tokenizer=None,type=''):
             return dataset, test_data
 
 
-
 def main():
     config = json.load(open('../../config/config.json'))
     outputpath = config['outputpath']
 
     wexea_directory = outputpath
 
-    dataset_dev,test_data_dev = get_dataset(wexea_directory, type='dev')
+    dataset_dev, test_data_dev = get_dataset(wexea_directory, type='dev')
     dataset_test, test_data_test = get_dataset(wexea_directory, type='test')
-    dataset_train,test_data_train = get_dataset(wexea_directory, type='train')
+    dataset_train, test_data_train = get_dataset(wexea_directory, type='train')
+
 
 if __name__ == "__main__":
     main()

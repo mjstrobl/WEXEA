7ba365fe771b83e6a0a84cd98fe648c5011017e3
Michael Strobl <mstrobl@ualberta.ca>
2021-11-29 09:31:28 +0100

    bug fix

--- new_src/el/candidate_tester.py
+++ new_src/el/candidate_tester.py
@@ -348,7 +348,7 @@ def process(documents, entity_start_token_id, id2title, title2id, title2filename
         input_ids = inputs['input_ids']
 
         entity_mask_tensors = []
-        b = torch.Tensor(len(input_ids), len(input_ids[0]), 768)
+        b = []
         for i in range(len(input_ids)):
             # for each mention
             entity_start_token = -1
@@ -357,18 +357,18 @@ def process(documents, entity_start_token_id, id2title, title2id, title2filename
                     entity_start_token = j
                     break
 
-            #entity_mask = [False] * len(input_ids[i])
-            #entity_mask[entity_start_token] = True
-            entity_mask_tensor = torch.zeros([len(input_ids[i]), 1], dtype=torch.bool)
-            entity_mask_tensor[entity_start_token] = torch.zeros(1).fill_(True)
+            entity_mask = [False] * len(input_ids[i])
+            entity_mask[entity_start_token] = True
+            #entity_mask_tensor = torch.zeros([len(input_ids[i]), 1], dtype=torch.bool)
+            #entity_mask_tensor[entity_start_token] = torch.zeros(1).fill_(True)
 
             '''entity_mask[entity_start_token] = True
             entity_mask_tensor = torch.zeros([len(input_ids[i]), 768], dtype=torch.bool)
             for j in range(len(input_ids[i])):
                 entity_mask_tensor[j] = torch.zeros(768).fill_(entity_mask[j])'''
-            b[i] = entity_mask_tensor
+            b.append(entity_mask)
 
-        inputs['entity_mask'] = b
+        inputs['entity_mask'] = torch.tensor(b, dtype=torch.bool)
         inputs['labels'] = torch.LongTensor([labels]).T
         inputs['priors'] = torch.FloatTensor([adds_priors]).T
         inputs['redirects'] = torch.FloatTensor([adds_redirects]).T

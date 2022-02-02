aab0788980a7c18074f8d376c62810d9e630b604
Michael Strobl <mstrobl@ualberta.ca>
2022-02-02 15:34:40 +0100

    Untrack files in .gitignore

--- annotate.sh
+++ annotate.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Start parser 1: Create dictionaries for titles, ids, redirects, categories, lists and store each article separately (including lists and categories)."
-python src/parser_1.py
+#python src/parser_1.py
 
 echo "Start parser 2: Separate disambiguation and stub articles; Remove Wikipedia markup; Prune dictionaries."
-python src/parser_2.py
+#python src/parser_2.py
 
 echo "Start parser 3: Add new annotations."
-python src/parser_3.py
+#python src/parser_3.py
 
 echo "Start parser 4: Co-reference resolution and entity linking."
 # python src/parser_4_greedy.py # For non-English Wikipedia dumps.
